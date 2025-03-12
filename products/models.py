d9fd17030aa79b5041a2a2d8db56c771b60176a1
furkan <furkan@cybercore.com.au>
2025-03-13 01:38:24 +1100

    updated products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model): # Product is a class that inherits from models.Model
+    name = models.CharField(max_length=255) #attributes of the class
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
